0b86c805417b300f36fead4789336e19aabca6fe
ChunYu <yo80106@gmail.com>
2021-04-23 23:31:30 +0100

    Add timeline folders.

---
+++ site/src/app/timeline-p1/timeline-p1.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+declare const on: any;
+declare const off: any;
+
+@Component({
+  selector: 'app-timeline-p1',
+  templateUrl: './timeline-p1.component.html',
+  styleUrls: ['./timeline-p1.component.css']
+})
+export class TimelineP1Component implements OnInit {
+
+  constructor() { 
+  }
+
+  ngOnInit(): void {
+  }
+
+}
