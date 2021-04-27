9cca71b3067fd3ddd47280dcfbaa94e236f801e4
ChunYu <yo80106@gmail.com>
2021-04-27 16:00:41 +0100

    Add funfacts-p2 for new menu page.

---
+++ site/src/app/funfacts-p2/funfacts-p2.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-funfacts-p2',
+  templateUrl: './funfacts-p2.component.html',
+  styleUrls: ['./funfacts-p2.component.css'],
+})
+export class FunFactsP2Component { }
+  
