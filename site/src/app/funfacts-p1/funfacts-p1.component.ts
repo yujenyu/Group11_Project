09265835db0837d6abe3cb2fd859bd6a628d100c
Chun-Yu Chen <yo80106@gmail.com>
2021-05-04 12:11:29 +0100

    Recover the deleted files.

---
+++ site/src/app/funfacts-p1/funfacts-p1.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+declare const on: any;
+declare const off: any;
+
+
+@Component({
+  selector: 'app-funfacts-p1',
+  templateUrl: './funfacts-p1.component.html',
+  styleUrls: ['./funfacts-p1.component.css'],
+})
+export class FunFactsP1Component implements OnInit{
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
