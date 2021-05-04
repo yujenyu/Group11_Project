4a7ce3724b0853f279fb3830646289299a2cbdec
ChunYu <yo80106@gmail.com>
2021-05-04 21:22:36 +0100

    Use art detective version of server.js.

--- site/server/server.js
+++ site/server/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Get dependencies
 const express = require('express');
 const path = require('path');
@@ -45,52 +44,4 @@ const server = http.createServer(app);
 /**
  * Listen on provided port, on all network interfaces.
  */
-=======
-// Get dependencies
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
-const db = require('./db');
-const TimelineRoutes = require('./routes/timeline-routes');
-const FunFactsRoutes = require('./routes/funfacts-routes');
-const ArtDetectiveRoutes = require('./routes/artdetective-routes');
-
-// // Get our API routes
-// const api = require('./api');
-
-const app = express();
-
-// // Parsers for POST data
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: false}));
-
-// Point static path to dist (folder where build files are located)
-app.use(express.static(path.join(__dirname, '../dist/demo')));
-
-// Set our api routes
-app.use('/timeline', TimelineRoutes);
-app.use('/funfacts', FunFactsRoutes);
-app.use('/artdetective', ArtDetectiveRoutes);
-
-// Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/index.html'));
-});
-
-/**
- * Get port from environment and store in Express.
- */
-const port = process.env.PORT || '3000';
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
->>>>>>> feature-artdetective
 server.listen(port, () => console.log(`API running on localhost:${port}`));
