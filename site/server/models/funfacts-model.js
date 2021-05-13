57ab6ee801b1f449b02beb84be913bf4b362719e
ChunYu <yo80106@gmail.com>
2021-05-13 10:46:47 +0100

    Clear master branch.

--- site/server/models/funfacts-model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const FunFactsSchema = new mongoose.Schema({
-    name: String,
-    url: String
-});
-
-module.exports = mongoose.model('FunFactsModel', FunFactsSchema);
--- site/src/app/artDetective/artDetective-p2/artDetective-p2.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-artDetective-p2',
-  templateUrl: './artDetective-p2.component.html',
-  styleUrls: ['./artDetective-p2.component.css']
-})
-export class ArtDetectiveP2Component {
-}
